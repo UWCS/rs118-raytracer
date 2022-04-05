9976e4934e525ffb1caf0c40fc48e8141fb814a2
Joey <josephharrison1@me.com>
2022-04-05 18:05:34 +0100

    Task 1.2 done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use image::{Rgb, RgbImage};
 fn main() {
-    let (width, height) = (256, 256);
+    let (width, height) = (600, 400);
     let mut buffer = RgbImage::new(width, height);
-    for (_, _, px) in buffer.enumerate_pixels_mut() {
-        *px = Rgb([255, 0, 0]);
+    for (i, j, px) in buffer.enumerate_pixels_mut() {
+        let r = i as f64 / (width - 1) as f64;
+        let g = j as f64 / (height - 1) as f64;
+        let b = 0.25;
+
+        *px = Rgb([r, g, b].map(|c| (c * 255.999) as u8))
     }
     buffer.save("render.png").expect("Could not save image");
 }
